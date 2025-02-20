e4f641109f4dc1fe98d444ead2d0ca89549395d3
Jacob Melso <jmelso@wgu.edu>
2025-02-20 13:03:40 -0600

    Create CodingController class for CRUD operations with CreateSession method

---
+++ CodingTracker/CodingTracker/CodingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SQLite;
+
+namespace CodingTracker
+{
+    static class CodingController
+    {
+
+        public static void CreateSession(DateTime start, DateTime? end = null)
+        {
+            var session = new CodingSession(start, end);
+
+            using (var connection = new SQLiteConnection(ConfigManager.ConnectionString))
+            {
+                connection.Execute(
+                    "INSERT INTO coding_tracker (StartTime, EndTime, Duration) VALUES (@StartTime, @EndTime, @Duration)",
+                    session
+                );
+            }
+        }
+
+    }
+}
