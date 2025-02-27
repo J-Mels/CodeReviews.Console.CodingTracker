1e43b75410b8a69dad9c71f5fafc182df67276bd
Jacob Melso <jmelso@wgu.edu>
2025-02-26 21:00:35 -0600

    Add TableVisualizationEngine class with DisplaySessions() method.

---
+++ CodingTracker/CodingTracker/TableVisualizationEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+
+namespace CodingTracker
+{
+    public class TableVisualizationEngine
+    {
+        public static void DisplaySessions()
+        {
+            var table = new Table();
+
+            table.AddColumn(new TableColumn("Id").Centered());
+            table.AddColumn("Start Time");
+            table.AddColumn("End Time");
+            table.AddColumn(new TableColumn("Duration").Centered());
+
+            foreach (var session in CodingController.GetAllSessions())
+            {
+                table.AddRow(
+                    session.Id.ToString(),
+                    session.StartTime.ToString("yyyy-MM-dd HH:mm"),
+                    session.EndTime?.ToString("yyyy-MM-dd HH:mm") ?? "[grey]N/A[/]",
+                    session.Duration.HasValue ? $"[green]{session.Duration.Value.ToString(@"hh\:mm\:ss")}[/]" : "[grey]N/A[/]"
+                    );
+            }
+
+            AnsiConsole.Write(table);
+        }
+    }
+}
