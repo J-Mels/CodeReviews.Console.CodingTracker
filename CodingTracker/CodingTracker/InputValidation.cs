0b54e2da33cd2c61bb97a795966b0ed891cdbd58
Jacob Melso <jmelso@wgu.edu>
2025-02-26 22:00:56 -0600

    Add InputValidation class with TryParseDateTime method.

---
+++ CodingTracker/CodingTracker/InputValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker
+{
+    public class InputValidation
+    {
+        public static bool TryParseDateTime(string userInput, string format, out DateTime dateTime)
+        {
+            return DateTime.TryParseExact(userInput, format, null, System.Globalization.DateTimeStyles.None, out dateTime);
+        }
+    }
+}
