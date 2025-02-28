975782ee635f7fe581bf3a74960a9c6128baef20
Jacob Melso <jmelso@wgu.edu>
2025-02-27 18:15:04 -0600

    Create main program menu structure

--- CodingTracker/CodingTracker/Program.cs
+++ CodingTracker/CodingTracker/Program.cs
@@ -1,10 +1,12 @@
 ﻿using CodingTracker;
+using Spectre.Console;
 using System;
 
 class Program
 {
     static void Main()
     {
+        bool programRunning = true;
         //try
         //{
         //    ConfigManager configManager = new ConfigManager();
@@ -18,22 +20,58 @@ class Program
 
         DatabaseInitializer.InitializeDatabase();
 
-        //try
-        //{
-            DateTime start = DateTime.Today;
-            DateTime end = DateTime.Now;
-            CodingSession codingSession = new CodingSession(start, end);
+        while (programRunning)
+        {
+            try
+            {
+                //DateTime start = DateTime.Today;
+                //DateTime end = DateTime.Now;
+                //CodingSession codingSession = new CodingSession(start, end);
 
-            Console.WriteLine(codingSession.StartTime);
-            Console.WriteLine(codingSession.EndTime);
-            Console.WriteLine(codingSession.Duration);
+                //Console.WriteLine(codingSession.StartTime);
+                //Console.WriteLine(codingSession.EndTime);
+                //Console.WriteLine(codingSession.Duration);
 
-            TableVisualizationEngine.DisplaySessions();
-        //}
-        //catch (Exception ex)
-        //{
-        //    Console.WriteLine($"Error: {ex.Message}");
-        //}
-        Console.ReadKey();
+
+                AnsiConsole.WriteLine("------MAIN MENU------");
+                AnsiConsole.WriteLine();
+                AnsiConsole.WriteLine("Select an option:");
+                AnsiConsole.WriteLine();
+                AnsiConsole.WriteLine("1) View all coding sessions");
+                AnsiConsole.WriteLine("2) Create coding session");
+                AnsiConsole.WriteLine("3) Update coding session");
+                AnsiConsole.WriteLine("4) Delete coding session");
+                AnsiConsole.WriteLine("0) Exit");
+
+                string? userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        TableVisualizationEngine.DisplaySessions();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "0":
+                        programRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine();
+                        break;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine("Press any key to continue ...");
+                Console.ReadKey();
+            }
+
+        }
     }
 }
